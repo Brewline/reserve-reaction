9a682b4710119bbc5fcd9b11dcc528286689831b
Pat Newell <pnewell+git@renttherunway.com>
2017-11-09 15:49:06 -0500

    Initial Commit

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import { ServiceConfigHelper } from "../../../core/accounts/client/helpers/util";
+
+ServiceConfigHelper.addProvider("Untappd", [
+  { property: "clientId", label: "Client ID" },
+  { property: "secret", label: "Client Secret" }
+]);
+
