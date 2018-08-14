42f46980639030d163a1478969f32d96dbacda7f
Pat Newell <pnewell+git@renttherunway.com>
2018-08-14 16:00:13 -0400

    Explicitly Hide Preloader

--- imports/plugins/custom/reaction-brewline-theme/client/index.js
+++ imports/plugins/custom/reaction-brewline-theme/client/index.js
@@ -1,1 +1,22 @@
+import { Router } from "/client/api";
+
 import "./favicon";
+
+function addBodyClass() {
+  if (addBodyClass.oneAndDone) { return; }
+
+  document.querySelector("body").setAttribute("data-ready", "true");
+
+  addBodyClass.oneAndDone = true;
+}
+
+// function logSomeContext(context) {
+//   console.log("The current route details...");
+//   console.log("Params: ", context.params);
+//   console.log("Query Params: ", context.queryParams);
+//   console.log("Path: ", context.path);
+//   console.log("The route object: ", context.route);
+// }
+
+// log out route details on every route
+Router.Hooks.onEnter(addBodyClass);
